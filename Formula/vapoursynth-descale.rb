fd904a2a2b27f4f912a9aaf9b52b9f78e0ee8b8e
Inflation <2375962+inflation@users.noreply.github.com>
2023-05-29 16:02:57 +0800

    Add `libdescale` vs plugin

---
+++ Formula/vapoursynth-descale.rb
@@ -0,0 +1,19 @@
+class VapoursynthDescale < Formula
+  desc "Video/Image filter to undo upscaling."
+  homepage "https://github.com/Irrational-Encoding-Wizardry/descale"
+  url "https://github.com/Irrational-Encoding-Wizardry/descale/archive/refs/tags/r8.tar.gz"
+  version "r8"
+  sha256 "317d955cc2dfbc3fd1aecef2ea2d56e4f1cd99434492d71c6a1d48213ff35972"
+  license "MIT"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+
+  depends_on "vapoursynth"
+
+  def install
+    system "meson", "build", *std_meson_args
+    system "ninja", "-C", "build"
+    (lib/"vapoursynth").install "build/libdescale.dylib"
+  end
+end
